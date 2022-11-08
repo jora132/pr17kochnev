7235809dd7a44f2fca203586baaea3a3673de2c4
jora132 <ko4nevg@yadex.ru>
2022-11-08 19:09:47 +0700

    first commit

---
+++ app/src/main/java/com/example/pr17german/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.pr17german;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.net.Uri;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    TextView tvOut;
+    Button btnOk;
+    Button btnCancel;
+    Button btnWeb;
+    Button btnMap;
+    Button btnCall;
+
+
+    private static final String TAG = "myLogs";
+
+    /** Called when the activity is first created. */
+    @SuppressLint("MissingInflatedId")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // найдем View-элементы
+        Log.d(TAG, "найдем View-элементы");
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        btnOk = (Button) findViewById(R.id.btnOk);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnWeb = (Button) findViewById(R.id.btnWeb);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnCall = (Button) findViewById(R.id.btnCall);
+        // присваиваем обработчик кнопкам
+        Log.d(TAG, "присваиваем обработчик кнопкам");
+        btnOk.setOnClickListener(this);
+        btnCancel.setOnClickListener(this);
+
+
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // по id определяем кнопку, вызвавшую этот обработчик
+        Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
+        switch (v.getId()) {
+            case R.id.btnOk:
+                // кнопка ОК
+                Log.d(TAG, "кнопка ОК");
+                tvOut.setText("Нажата кнопка ОК");
+                Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnCancel:
+                // кнопка Cancel
+                Log.d(TAG, "кнопка Cancel");
+                tvOut.setText("Нажата кнопка Cancel");
+                Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnWeb:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:55.754283,37.62002"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
+    }
+
+}
